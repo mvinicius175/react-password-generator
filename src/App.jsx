165e58349eac1343c8da69ea64d450353869177d
mvinicius175 <mauro.vinicius175@gmail.com>
2024-01-23 12:29:38 -0300

    html and css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [lenght, setLenght] = useState(8);
+	const [numberAllowed, setNumberAllowed] = useState(false);
+	const [charactersAllowed, setCharactersAllowed] = useState(false);
+	const [password, setPassword] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="w-full max=w=md mx-auto shadow-md rounded-lg px-4 py-3 bg-gray-800 text-orange-500">
+			<h1 className="text-white text-center my-3">Password Generator</h1>
+			<div className="flex shadow rounded-lg overflow-hidden mb-4">
+				<input
+					type="text"
+					value={password}
+					className="outline-none w-full py-1 px-3"
+					placeholder="Password"
+					readOnly
+				/>
+				<button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+					Copy
+				</button>
+			</div>
+			<div className="flex text-sm gap-x-2">
+				<div className="flex items-center gap-x-1">
+					<input
+						type="range"
+						min={6}
+						max={30}
+						value={lenght}
+						className="cursor-pointer"
+						onChange={(e) => setLenght(e.target.value)}
+						name=""
+						id=""
+					/>
+					<label htmlFor="lenght">Lenght: {lenght}</label>
+				</div>
+
+				<div className="flex items-center gap-x-1">
+					<input
+						type="checkbox"
+						defaultChecked={numberAllowed}
+						onChange={() => {
+							setNumberAllowed((prev) => !prev);
+						}}
+						name=""
+						id=""
+					/>
+					<label htmlFor="number">Numbers</label>
+				</div>
+				<div className="flex items-center gap-x-1">
+					<input
+						type="checkbox"
+						defaultChecked={charactersAllowed}
+						onChange={() => {
+							setCharactersAllowed((prev) => !prev);
+						}}
+						name=""
+						id=""
+					/>
+					<label htmlFor="charInput">Characters</label>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
